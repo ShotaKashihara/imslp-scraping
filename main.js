d6bcaf0651bec7c3c06dd77cd1cdbc536465ee48
Shota Kashihara <mercury4th@gmail.com>
2020-01-12 22:06:22 +0900

    init

---
+++ main.js
@@ -0,0 +1,68 @@
+const rp = require('request-promise')
+const promiseMap = require('bluebird').map
+const { JSDOM } = require('jsdom')
+const fs = require('fs')
+const sanitizeHtml = require('sanitize-html');
+global.Element = (new JSDOM()).window.Element;
+Object.defineProperty(global.Element.prototype, 'innerText', {
+  get() {
+    return sanitizeHtml(this.innerHTML, {
+      allowedTags: [ 'br' ], // remove all tags and return text content only
+      allowedAttributes: {}, // remove all tags and return text content only
+    });
+  },
+  configurable: true, // make it so that it doesn't blow chunks on re-running tests with things like --watch
+});
+
+// 
+// For Orchestra
+// https://imslp.org/wiki/List_of_Orchestra_Pieces_with_Parts_Available
+// 
+const titles = [
+  "https://imslp.org/wiki/Symphony_No.3%2C_Op.55_(Beethoven%2C_Ludwig_van)",
+  "https://imslp.org/wiki/Symphony_No.4%2C_Op.60_(Beethoven%2C_Ludwig_van)",
+  "https://imslp.org/wiki/Symphony_No.5,_Op.67_%28Beethoven,_Ludwig_van%29",
+  "https://imslp.org/wiki/Symphony_No.6%2C_Op.68_(Beethoven%2C_Ludwig_van)",
+  "https://imslp.org/wiki/Symphony_No.7%2C_Op.92_(Beethoven%2C_Ludwig_van)",
+  "https://imslp.org/wiki/Symphony_No.8%2C_Op.93_(Beethoven%2C_Ludwig_van)",
+  "https://imslp.org/wiki/Symphony_No.9%2C_Op.125_(Beethoven%2C_Ludwig_van)",
+]
+
+async function getTable(title) {
+  console.log(`${title}: Requesting...`)
+  return await rp(`${title}`)
+    .then(body => {
+      console.log('Finish.')
+      const document = new JSDOM(body).window.document
+      return Array.from(document.querySelectorAll('.wi_body > table > tbody > tr'), r => {
+        return {
+          "key": r.querySelector('th').textContent.trim(),
+          "value": r.querySelector('td').innerText.replace(/<br \/>/g, '\n').trim()
+        }
+      })
+    })
+}
+
+const main = async () => {
+  return await promiseMap(
+    titles,
+    title => getTable(title),
+    { concurrency: 2 }
+  )
+}
+
+(async () => {
+  const result = await main()
+  fs.writeFile('imslp.json', JSON.stringify(result, null, 2), 'utf8', () => {})
+
+  // result.forEach(r => {
+  //   r.forEach(r2 => {
+  //     if(r2.key === "Work Title") {
+  //       console.log(r2.value)
+  //     }
+  //     if(r2.key === "Instrumentation") {
+  //       console.log(r2.value)
+  //     }
+  //   })
+  // })
+})()
