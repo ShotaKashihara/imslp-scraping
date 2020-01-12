e0b83e54451135aeb449119e0243136ba6192de4
Shota Kashihara <mercury4th@gmail.com>
2020-01-12 22:27:32 +0900

    add genre.

--- main.js
+++ main.js
@@ -20,13 +20,22 @@ Object.defineProperty(global.Element.prototype, 'innerText', {
 // 
 const titles = [
   "https://imslp.org/wiki/Symphony_No.3%2C_Op.55_(Beethoven%2C_Ludwig_van)",
-  "https://imslp.org/wiki/Symphony_No.4%2C_Op.60_(Beethoven%2C_Ludwig_van)",
-  "https://imslp.org/wiki/Symphony_No.5,_Op.67_%28Beethoven,_Ludwig_van%29",
-  "https://imslp.org/wiki/Symphony_No.6%2C_Op.68_(Beethoven%2C_Ludwig_van)",
-  "https://imslp.org/wiki/Symphony_No.7%2C_Op.92_(Beethoven%2C_Ludwig_van)",
-  "https://imslp.org/wiki/Symphony_No.8%2C_Op.93_(Beethoven%2C_Ludwig_van)",
-  "https://imslp.org/wiki/Symphony_No.9%2C_Op.125_(Beethoven%2C_Ludwig_van)",
+  // "https://imslp.org/wiki/Symphony_No.4%2C_Op.60_(Beethoven%2C_Ludwig_van)",
+  // "https://imslp.org/wiki/Symphony_No.5,_Op.67_%28Beethoven,_Ludwig_van%29",
+  // "https://imslp.org/wiki/Symphony_No.6%2C_Op.68_(Beethoven%2C_Ludwig_van)",
+  // "https://imslp.org/wiki/Symphony_No.7%2C_Op.92_(Beethoven%2C_Ludwig_van)",
+  // "https://imslp.org/wiki/Symphony_No.8%2C_Op.93_(Beethoven%2C_Ludwig_van)",
+  // "https://imslp.org/wiki/Symphony_No.9%2C_Op.125_(Beethoven%2C_Ludwig_van)",
 ]
+
+const music = {
+  url: "http://imslp.org",
+  composer: "Beethoven",
+  title: "Symphony No.3, Op.67",
+  infomation: {
+
+  }
+}
 
 async function getTable(title) {
   console.log(`${title}: Requesting...`)
@@ -34,7 +43,9 @@ async function getTable(title) {
     .then(body => {
       console.log('Finish.')
       const document = new JSDOM(body).window.document
-      return Array.from(document.querySelectorAll('.wi_body > table > tbody > tr'), r => {
+      const wp_header = Array.from(document.querySelectorAll('.wp_header > table > tbody > tr'))
+      const wi_body = Array.from(document.querySelectorAll('.wi_body > table > tbody > tr'))
+      return wp_header.concat(wi_body).map(r => {
         return {
           "key": r.querySelector('th').textContent.trim(),
           "value": r.querySelector('td').innerText.replace(/<br \/>/g, '\n').trim()
